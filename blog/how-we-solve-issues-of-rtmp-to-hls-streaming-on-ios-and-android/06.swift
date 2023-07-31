ac66667ff3ef18a56a40a7d04262362107022029
Yan-Avstreikh <106074138+Yan-Avstreikh@users.noreply.github.com>
2023-07-31 16:23:33 +0400

    Update 06.swift

--- blog/how-we-solve-issues-of-rtmp-to-hls-streaming-on-ios-and-android/06.swift
+++ blog/how-we-solve-issues-of-rtmp-to-hls-streaming-on-ios-and-android/06.swift
@@ -1,6 +1,6 @@
 func rtmpStream(_ stream: RTMPStream, didPublishSufficientBW connection: RTMPConnection) { 
-   // Метод вызывается каждую секунду при достаточной пропускной способности, используется для увеличения битрейта 
+   // The method is called every second with sufficient bandwidth, used to increase the bitrate 
 } 
 func rtmpStream(_ stream: RTMPStream, didPublishInsufficientBW connection: RTMPConnection) { 
-   // Метод вызывается при недостаточной пропускной способности, используется для снижения битрейта 
+   // The method is called when there is insufficient bandwidth, used to reduce the bitrate 
 } 
